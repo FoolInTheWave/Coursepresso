b236eadd23de4e502e773d97b8a9b15e10036b44
SteveFoco <sjfoco@svsu.edu>
2015-02-21 15:34:45 -0500

    Edit Course Section Finished

--- coursepresso/src/main/java/com/coursepresso/project/repository/CourseSectionRepository.java
+++ coursepresso/src/main/java/com/coursepresso/project/repository/CourseSectionRepository.java
@@ -2,7 +2,9 @@ package com.coursepresso.project.repository;
 
 import java.util.List;
 import com.coursepresso.project.entity.CourseSection;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -20,4 +22,14 @@ public interface CourseSectionRepository extends CrudRepository<CourseSection, I
    * @return A List of CourseSection records as CourseSection objects.
    */
   List<CourseSection> findBySectionNumber(int sectionNumber);
+  
+  /**
+   * Custom FIND method retrieves a Department record from the database with an
+   * initialized list of Professor records mapped by a foreign key.
+   * 
+   * @param name The name to match.
+   * @return A Department record as a Department object.
+   */
+  @Query("SELECT cs FROM CourseSection cs JOIN FETCH cs.meetingDayList WHERE cs.id = (:id)")
+  public CourseSection findByIdWithMeetingDays(@Param("id") Integer id);
 }
