a80d95343523937b2d3b22a554474edb68012a64
SteveFoco <sjfoco@svsu.edu>
2015-03-29 16:10:44 -0400

    YAY FOR MENUBARS

--- coursepresso/src/main/java/com/coursepresso/project/controller/MainController.java
+++ coursepresso/src/main/java/com/coursepresso/project/controller/MainController.java
@@ -4,7 +4,8 @@ import com.coursepresso.project.entity.CourseSection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 
@@ -19,7 +20,9 @@ public class MainController {
   @FXML
   private BorderPane contentArea;
   @FXML
-  private Button logout;
+  private MenuItem logoutMnu;
+  @FXML
+  private MenuItem aboutMnu;
 
   @Inject
   private NewCourseSectionController newCourseSectionController;
@@ -47,8 +50,18 @@ public class MainController {
   }
 
   @FXML
-  private void logoutButtonClick(ActionEvent event) {
+  private void logoutMnuClick(ActionEvent event) {
     loginController.logout();
+  }
+
+  @FXML
+  private void aboutMnuClick(ActionEvent event) {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setTitle("Information Dialog");
+    alert.setHeaderText("Look, an Information Dialog");
+    alert.setContentText("I have a great message for you!");
+
+    alert.showAndWait();
   }
 
   public void showNewCourseSection() {
